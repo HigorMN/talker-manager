4f567f8f34d4c0e9e4ee961e3efb37381e9af9df
HigorMN <higor.maranhao2000@gmail.com>
2022-12-14 20:07:00 -0300

    feat(Requisito 3): criando endpoint POST /login

--- src/routes/httpStatus.js
+++ src/routes/httpStatus.js
@@ -1,2 +1,7 @@
-export const HTTP_OK_STATUS = 200;
-export const HTTP_NOT_FOUND_STATUS = 404;
+const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
+
+module.exports = {
+  HTTP_OK_STATUS,
+  HTTP_NOT_FOUND_STATUS,
+};
--- src/routes/login.router.js
+++ src/routes/login.router.js
@@ -1,5 +1,14 @@
 const express = require('express');
 
+const { HTTP_OK_STATUS } = require('./httpStatus');
+
 const router = express.Router();
+
+router.post('/login', (_req, res) => {
+  const token = (Math.random().toString(16).substring(2) 
+  + Math.random().toString(16).substring(2)).substring(0, 16);
+
+  res.status(HTTP_OK_STATUS).json({ token });
+});
 
 module.exports = router;
