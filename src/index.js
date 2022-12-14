f06f3f1e5b98d1ea6645f0651e333f35aee9f1ef
HigorMN <higor.maranhao2000@gmail.com>
2022-12-14 18:36:44 -0300

    feat(Requisito 2): criando endpoint GET /talker/:id

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 
 // não remova esse endpoint, e para o avaliador funcionar a
 app.get('/', (_request, response) => {
@@ -13,6 +14,20 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_req, res) => {
   res.status(HTTP_OK_STATUS).json(await readTalkerData());
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await readTalkerData();
+  const findId = data.find((talker) => +talker.id === +id);
+
+  if (!findId) {
+    return res.status(HTTP_NOT_FOUND_STATUS).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+
+  return res.status(HTTP_OK_STATUS).json(findId);
 });
 
 module.exports = app;
