acbe73266c825664ba62b406bd69af2388110fe6
HigorMN <higor.maranhao2000@gmail.com>
2022-12-14 16:58:59 -0300

    refactor: criando pasta server para indentificar o servidor

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar a
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
+module.exports = app;
