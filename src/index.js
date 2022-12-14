a67209c6ebfeeab5e4df1574149f7956fff112b5
HigorMN <higor.maranhao2000@gmail.com>
2022-12-14 19:11:25 -0300

    refactor: middleware de rotas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const routers = require('./routes');
+const { HTTP_OK_STATUS } = require('./routes/httpStatus');
 
 const app = express();
 app.use(express.json());
-
-const HTTP_OK_STATUS = 200;
 
 // não remova esse endpoint, e para o avaliador funcionar a
 app.get('/', (_request, response) => {
--- src/routes/talker.router.js
+++ src/routes/talker.router.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const { readTalkerData, findTalkerId } = require('../util/fsUtilsTalker');
+const { HTTP_OK_STATUS, HTTP_NOT_FOUND_STATUS } = require('./httpStatus');
 
 const router = express.Router();
-
-const HTTP_OK_STATUS = 200;
-const HTTP_NOT_FOUND_STATUS = 404;
 
 router.get('/talker', async (_req, res) => {
   res.status(HTTP_OK_STATUS).json(await readTalkerData());
